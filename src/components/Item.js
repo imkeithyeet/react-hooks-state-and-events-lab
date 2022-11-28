d6b8ad46b71190f4fc9608b8b336a6b2253bf39c
Keith Ma <keithchezzy@gmail.com>
2022-11-28 00:10:19 -0800

    statelab

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const[isInCart,setIsInCart] = useState(false)
+
+  function handleAddToCartClick() {
+    setIsInCart((isInCart)=>!isInCart)
+  }
+  
+  // const addToCart =isInCart ? "add" : "remove"
+  
   return (
-    <li className="">
+    <li  className={isInCart ? "add" : ""}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button 
+      className={isInCart ? "add": "remove"}
+      onClick={handleAddToCartClick}
+      >
+        {isInCart ? "addToCart ": "removeFromCart"}
+      </button>
     </li>
   );
 }
